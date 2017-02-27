9101598d891148917bdc8e85b881bdf73315e5db
Deon Kim osX <dae.h.kim@mail.mcgill.ca>
2017-02-27 17:35:31 -0500

    first commit

---
+++ stopwatch.py
@@ -0,0 +1,25 @@
+import time
+
+def stopwatch(seconds):
+    start = time.time()
+    time.clock()    
+    count_file = open("secs.txt","r")
+    lines = count_file.readlines()
+    count_file.close()
+    elapsed_init = int(lines[0])
+    while (True):
+        elapsed = time.time() - start + elapsed_init
+        m, s = divmod(elapsed, 60)
+        h, m = divmod(m, 60)
+        print("%d:%02d:%02d" % (h,m,s))
+        print("loop cycle time: %f, seconds count: %d" % (time.clock() , elapsed))
+        text_file = open("stopwatch.txt","w")
+        text_file.write("%d:%02d:%02d\n" % (h,m,s))
+        text_file.close()
+        count_file = open("secs.txt","w") 
+        count_file.write(str(int(elapsed)))
+        count_file.close()
+        time.sleep(1)  
+
+
+stopwatch(20)
